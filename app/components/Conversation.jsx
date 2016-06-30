6fc87cc0bb6e616fb37d7bcdec373badfd52b35c
Carlos Saito <c.saito.style@gmail.com>
2016-06-30 12:29:43 +0200

    Semantic HTML and better CSS

--- app/components/Conversation.jsx
+++ app/components/Conversation.jsx
@@ -4,12 +4,15 @@ import NewMessage from 'components/NewMessage';
 import styles from 'components/Conversation.scss';
 
 export default ({ messages }) => (
-  <div className={ styles.root }>
-    <div className={ styles.messageList }>
+  <div className={ styles.container }>
+    <header className={ styles.header }>
+      Messages
+    </header>
+    <main className={ styles.body }>
       <MessageList messages={messages} />
-    </div>
-    <div className={ styles.newMessage }>
+    </main>
+    <footer className={ styles.footer }>
       <NewMessage />
-    </div>
+    </footer>
   </div>
 );
--- app/components/NewMessage.jsx
+++ app/components/NewMessage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from 'components/NewMessage.scss';
 
 export default () => (
-  <form className={ styles.root }>
-    <textarea className={ styles.input } />
+  <form className={ styles.container }>
+    <input className={ styles.input } type="text" />
     <input className={ styles.submit } type="submit" name="submit" value="OK" />
   </form>
 );
