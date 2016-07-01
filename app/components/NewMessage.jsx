608633c5813c1bd32b339bc0f345f44ca7197929
Carlos Saito <c.saito.style@gmail.com>
2016-07-01 12:03:38 +0200

    CSS enhancement

--- app/components/NewMessage.jsx
+++ app/components/NewMessage.jsx
@@ -3,7 +3,7 @@ import styles from 'components/NewMessage.scss';
 
 export default () => (
   <form className={ styles.container }>
-    <input className={ styles.input } type="text" />
+    <input className={ styles.input } type="text" placeholder="Write your message..." />
     <input className={ styles.submit } type="submit" name="submit" value="OK" />
   </form>
 );
