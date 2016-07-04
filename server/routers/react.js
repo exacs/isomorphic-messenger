9384b3eb89cacd27600173ca4db145f2ef57dcf6
Carlos Saito <c.saito.style@gmail.com>
2016-07-04 02:35:41 +0200

    Include client-side scripting

--- server/routers/react.js
+++ server/routers/react.js
@@ -7,19 +7,23 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import Root from 'containers/Root';
 import createStore from 'store/configureStore';
+import { SUCCESS } from 'reducers/index';
 import { read } from '../logic/messages';
 
 export default (req, res) => {
   read().then(initialState => {
-    const store = createStore(initialState);
+    const store = createStore(initialState.map(
+      (message, i) => ({ text: message, id: i, status: SUCCESS })
+    ));
 
     res.set('Content-Type', 'text/html');
     res.send(`
       <!DOCTYPE html>
-      <link rel=stylesheet href=main.css>
+      <link rel=stylesheet href=/main.css>
       <title>An isomorphic application!!</title>
       <div id=root>${renderToString(<Root store={store} />)}</div>
       <script>window.__INITIAL_STATE__ = ${JSON.stringify(store.getState())}</script>
+      <script src=/bundle.js></script>
     `);
   });
 };
