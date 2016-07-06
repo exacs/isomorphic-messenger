6a2e62a084ecc9486ce1e423ab7f2d62a9c1c147
Carlos Saito <c.saito.style@gmail.com>
2016-07-06 20:58:29 +0200

    Fix. Postgres queries

--- server/data/postgres.js
+++ server/data/postgres.js
@@ -5,21 +5,37 @@
 import pg from 'pg';
 pg.defaults.ssl = true;
 
-const client = new pg.Client(process.env.DATABASE_URL);
+const connectionString = process.env.DATABASE_URL || {
+  user: 'carlos',
+  databse: 'minimal_messenger',
+  password: 'carlos',
+  port: 5432,
+};
+
+const client = new pg.Client(connectionString);
 
 export const getMessages = (limit = 10) => new Promise((accept, reject) => {
-  const err = client.connect();
-  if (err) reject('Connection error');
-
-  client.query('SELECT * FROM messages LIMIT $1', [limit])
-        .then(res => {
-          accept(res.rows.map(row => row.text));
-        })
-        .then(() => client.end());
+  client.connect(connErr => {
+    if (connErr) {
+      reject('Error on connection. Code:', connErr.code);
+    } else {
+      client.query('SELECT * FROM messages')
+            .then(res => accept(res.rows.map(row => row.text)))
+            .catch(() => reject('Error on SELECT'))
+            .then(() => client.end());
+    }
+  });
 });
 
-export const createMessage = (text) => new Promise((accept) => {
-  client.query('INSERT INTO messages (text) VALUES ($1)', [text])
-        .then(res => accept(res))
-        .then(() => client.end());
+export const createMessage = (text) => new Promise((accept, reject) => {
+  client.connect(connErr => {
+    if (connErr) {
+      reject('Error on connection. Code:', connErr.code);
+    } else {
+      client.query('INSERT INTO messages (text) values ($1)', [text])
+            .then(res => accept(res))
+            .catch(() => reject('Error on INSERT query'))
+            .then(() => client.end());
+    }
+  });
 });
