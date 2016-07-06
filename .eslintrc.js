6fb67beeffe67979182b083f5065838e39177030
Carlos Saito <c.saito.style@gmail.com>
2016-07-07 00:10:06 +0200

    Fix eslint configuration file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   'extends': 'airbnb',
   'rules': {
-    'no-underscore-dangle': ['error', {
+    'no-underscore-dangle': [2, {
       'allow': ['__INITIAL_STATE__']
     }],
     'import/no-unresolved': 'off',
