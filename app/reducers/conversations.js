f5c77740d94f6958bf4c9ed072e8b6b93ca07f5f
Carlos Saito <c.saito.style@gmail.com>
2016-07-12 20:49:15 +0200

    Create a new conversation if not exists

--- app/reducers/conversations.js
+++ app/reducers/conversations.js
@@ -1,7 +1,24 @@
+import { SEND_MESSAGE_REQUEST } from 'actions/index';
+
 const INITIAL_STATE = {};
 
-function conversationsReducer(conversations = INITIAL_STATE, action) {
-  return conversations;
+function addMessage(conversation, messageKey) {
+  return ({
+    messages: conversation && conversation.messages ?
+              conversation.messages.concat(messageKey) :
+              [messageKey],
+  });
 }
 
-export default conversationsReducer
+function conversationsReducer(conversations = INITIAL_STATE, action) {
+  const newChat = addMessage(conversations[action.chatId], action.key);
+
+  switch (action.type) {
+    case SEND_MESSAGE_REQUEST:
+      return Object.assign({}, conversations, { [action.chatId]: newChat });
+    default:
+      return conversations;
+  }
+}
+
+export default conversationsReducer;
